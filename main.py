2a0120edc96ea012a5f39f5d5ce6428a9bec7a55
GitOutlaw <jdcaius@protonmail.com>
2025-03-31 20:46:48 -0400

    created the dt variable to the for loop dt = fps.tick(60) / 1000 this will increase performance

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ from constants import *
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+fps = pygame.time.Clock()
+dt = 0
+
+
 def main():
     print('Starting Asteroids!')
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     game_running = True
-
-
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+        dt = fps.tick(60) / 1000
         screen.fill("BLACK")
         pygame.display.flip()
-
-
-
 
 
 if __name__ == "__main__":
