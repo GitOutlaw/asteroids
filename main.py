8b17f843058099ddaf0d27d605692c3668cce5b3
GitOutlaw <jdcaius@protonmail.com>
2025-04-01 11:24:17 -0400

    updated the contants.py file and create the player.py file which holds the player data and function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player 
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +14,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player_x = SCREEN_WIDTH // 2
+    player_y = SCREEN_HEIGHT // 2
+    player = Player(player_x, player_y)
+
     game_running = True
     while game_running:
         for event in pygame.event.get():
@@ -20,6 +25,7 @@ def main():
                 return
         dt = fps.tick(60) / 1000
         screen.fill("BLACK")
+        player.draw(screen)
         pygame.display.flip()
 
 
