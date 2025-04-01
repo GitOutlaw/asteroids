7595beb196d85c549d48a9c68f06041a151b6ce6
GitOutlaw <jdcaius@protonmail.com>
2025-03-31 20:26:42 -0400

    created base file for asteroids game

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    print('Starting Asteroids!')
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    game_running = True
+
+
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("BLACK")
+        pygame.display.flip()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
